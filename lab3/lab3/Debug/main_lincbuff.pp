822205701c3e31a143bbea7e3f2b792bbf45037d
Dimple Kochar <dimplekochar99@gmail.com>
2019-06-03 13:58:30 +0200

    Add files via upload

---
+++ lab3/lab3/Debug/main_lincbuff.pp
@@ -0,0 +1,10 @@
+# FIXED
+main_lincbuff.obj: ../main_lincbuff.c
+main_lincbuff.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/stdio.h
+main_lincbuff.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/linkage.h
+main_lincbuff.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/stdarg.h
+main_lincbuff.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/emulation/boards/usbstk5515_v1/include/usbstk5515.h
+main_lincbuff.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/emulation/boards/usbstk5515_v1/include/usbstk5515_gpio.h
+main_lincbuff.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/emulation/boards/usbstk5515_v1/include/usbstk5515.h
+main_lincbuff.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/emulation/boards/usbstk5515_v1/include/usbstk5515_i2c.h
+main_lincbuff.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/emulation/boards/usbstk5515_v1/include/usbstk5515.h
---
+++ lab4/Debug/subdir.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main_circbuff.c 
+
+ASM_SRCS += \
+../asmcircbuff.asm 
+
+CMD_SRCS += \
+../lnkx.cmd 
+
+OBJS += \
+./asmcircbuff.obj \
+./main_circbuff.obj 
+
+C_DEPS += \
+./main_circbuff.pp 
+
+OBJS__QTD += \
+".\asmcircbuff.obj" \
+".\main_circbuff.obj" 
+
+C_DEPS__QTD += \
+".\main_circbuff.pp" 
+
+ASM_SRCS_QUOTED += \
+"../asmcircbuff.asm" 
+
+C_SRCS_QUOTED += \
+"../main_circbuff.c" 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+asmcircbuff.obj: ../asmcircbuff.asm $(GEN_OPTS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: Compiler'
+	"C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/c5500/bin/cl55" -v5515 -g --include_path="C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/c5500/include" --include_path="C:/Program Files (x86)/Texas Instruments/ccsv4/emulation/boards/usbstk5515_v1/include" --diag_warning=225 --ptrdiff_size=32 --memory_model=large $(GEN_OPTS_QUOTED) $(subst #,$(wildcard $(subst $(SPACE),\$(SPACE),$<)),"#")
+	@echo 'Finished building: $<'
+	@echo ' '
+
+main_circbuff.obj: ../main_circbuff.c $(GEN_OPTS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: Compiler'
+	"C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/c5500/bin/cl55" -v5515 -g --include_path="C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/c5500/include" --include_path="C:/Program Files (x86)/Texas Instruments/ccsv4/emulation/boards/usbstk5515_v1/include" --diag_warning=225 --ptrdiff_size=32 --memory_model=large --preproc_with_compile --preproc_dependency="main_circbuff.pp" $(GEN_OPTS_QUOTED) $(subst #,$(wildcard $(subst $(SPACE),\$(SPACE),$<)),"#")
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
